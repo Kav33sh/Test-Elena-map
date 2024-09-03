b5d16f5c3592411ec192d96387f5a1dbb3f206e0
Kav33sh <94919346+Kav33sh@users.noreply.github.com>
2024-09-03 14:35:45 +0530

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,7 +1,7 @@
-// Initialize the map
+//Initialize the map
 var map = L.map('map').fitWorld();
 
-// Add a tile layer from OpenStreetMap
+//tile layer from OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
 }).addTo(map);
@@ -10,9 +10,9 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var destination = L.latLng(13.069706, 77.582314);
 
 // Locate the user's current position and set the view
-map.locate({setView: true, maxZoom: 15});
+map.locate({ setView: true, maxZoom: 15 });
 
-// Add routing control from the user's location to the destination
+// Routing control from the user's location to the destination
 function onLocationFound(e) {
     L.Routing.control({
         waypoints: [
@@ -23,16 +23,21 @@ function onLocationFound(e) {
         geocoder: L.Control.Geocoder.nominatim() // Adding geocoder control
     }).addTo(map);
 
-    // Add a marker at the destination
+    //marker at the destination
     L.marker(destination).addTo(map)
         .bindPopup("Destination: 13.084447, 77.594912").openPopup();
 }
 
-// Handle location errors
+//location errors
 function onLocationError(e) {
     alert("Unable to retrieve your location. " + e.message);
 }
 
-// Event listeners for location found and error handling
+//loading screen after 5 seconds
+setTimeout(function() {
+    document.getElementById('loading-screen').style.display = 'none';
+}, 5000); // 5000 milliseconds = 5 seconds
+
+// Locations
 map.on('locationfound', onLocationFound);
 map.on('locationerror', onLocationError);
